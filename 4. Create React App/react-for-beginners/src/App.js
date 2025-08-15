5f63ed5661ba35a335fc1f50ef9f6ba7b3b28902
radder214 <garden940316@gmail.com>
2025-08-15 17:12:41 +0900

    logo 관련 에러 로그가 자꾸 떠서 귀여운 옴팡이 이미지 추가함

--- 4. Create React App/react-for-beginners/src/App.js
+++ 4. Create React App/react-for-beginners/src/App.js
@@ -1,11 +1,26 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
+// chapter 4. CRA
+// function App() {
+//   return (
+//     <div>
+//       <h1 className={styles.title}>Welcome back!</h1>
+//       <Button text={"continue"}/>
+//     </div>
+//   );
+// }
+
+// chapter 5. Effects
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount(current => current + 1);
+  console.log("render");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"continue"}/>
+      <h1>count</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
