780bd286d3c797abe596fa3a36ecce7e4a624b92
radder214 <garden940316@gmail.com>
2025-08-15 17:33:45 +0900

    useEffect

--- 4. Create React App/react-for-beginners/src/App.js
+++ 4. Create React App/react-for-beginners/src/App.js
@@ -1,6 +1,28 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// chapter 5. Effects
+function App() {
+  // state
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount(current => current + 1);
+  // 매번 실행되는 코드
+  console.log("I run all the time");
+  // 딱 한 번만 실행하고 싶은 코드
+  useEffect(() => {
+    console.log("I run only once");
+  }, []); 
+
+  return (
+    <div>
+      <h1>count</h1>
+      <button onClick={onClick}>click</button>
+    </div>
+  );
+}
+
+export default App;
 
 // chapter 4. CRA
 // function App() {
@@ -11,18 +33,3 @@ import { useState } from "react";
 //     </div>
 //   );
 // }
-
-// chapter 5. Effects
-function App() {
-  const [count, setCount] = useState(0);
-  const onClick = () => setCount(current => current + 1);
-  console.log("render");
-  return (
-    <div>
-      <h1>count</h1>
-      <button onClick={onClick}>click</button>
-    </div>
-  );
-}
-
-export default App;
--- 4. Create React App/react-for-beginners/src/index.js
+++ 4. Create React App/react-for-beginners/src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // Strict 모드 다시 적용하고 싶으면 아래 주석들을 풀어라
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
