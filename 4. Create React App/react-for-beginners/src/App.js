edeac1bf71ca05b0654be579807a4e2315b50f81
radder214 <garden940316@gmail.com>
2025-08-15 18:29:17 +0900

    useEffect - Deps

--- 4. Create React App/react-for-beginners/src/App.js
+++ 4. Create React App/react-for-beginners/src/App.js
@@ -6,17 +6,45 @@ import { useState, useEffect } from "react";
 function App() {
   // state
   const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCount(current => current + 1);
-  // 매번 실행되는 코드
-  console.log("I run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
+
   // 딱 한 번만 실행하고 싶은 코드
   useEffect(() => {
     console.log("I run only once");
-  }, []); 
+  }, []);
+  
+  // count state 값이 변경될 때만 아래 함수를 실행하고 싶다.
+  useEffect(() => {
+    // 해당 컴포넌트가 맨 처음 렌더링 될 때 함수가 실행되는 것 방지
+    // by 아래 조건문이 없으면 불필요한 함수를 실행하게 되므로
+    if(count > 0) {
+      console.log("I run when 'count' changes");
+    }
+  }, [count]);
+
+  // keyword state 값이 변경될 때만 아래 함수를 실행하고 싶다.
+  useEffect(() => {
+    // 해당 컴포넌트가 맨 처음 렌더링 될 때 함수가 실행되는 것 방지
+    // by 아래 조건문이 없으면 불필요한 검색 API를 실행하게 되므로
+    if(keyword !== "") {
+      console.log("I run when 'keyword' changes");
+    }
+  }, [keyword]);
+
+  // 복수의 state를 넣을수도 있음
+  useEffect(() => {
+    if(count > 0 || keyword !== "") {
+        // 로그 찍히는게 너무 많아서 주석 처리함
+        // console.log("I run when 'count' or 'keyword' changes");
+      }
+  }, [count, keyword]);
 
   return (
     <div>
-      <h1>count</h1>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here"></input>
+      <h1>{count}</h1>
       <button onClick={onClick}>click</button>
     </div>
   );
