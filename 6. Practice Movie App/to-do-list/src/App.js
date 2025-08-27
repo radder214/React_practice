4b70c88d78e3a19396296495c6077d7955564cea
radder214 <garden940316@gmail.com>
2025-08-27 21:25:12 +0900

    to do list - practice2

--- 6. Practice Movie App/to-do-list/src/App.js
+++ 6. Practice Movie App/to-do-list/src/App.js
@@ -30,7 +30,6 @@ function App() {
     });
     setToDo("");
   }
-  console.log(toDos);
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -42,6 +41,29 @@ function App() {
           placeholder="Write your to do"/>
         <button>Add To Do</button>
       </form>
+      <hr/>
+      <ul>
+        {
+          toDos.map((item, index) => {
+            // Q. 이미 바깥에 {중괄호}로 "여기는 JS 영역"이라는 것을 표현했는데 왜 그냥 item 이 아니라 {item}이라고 작성해야 하는 걸까?
+            // A.
+            // return <li>...</li> 문법을 사용한 순간 이 부분도 JSX 영역이 된다.
+            // 따라서 item 이라는 '변수'값을 화면에 뿌려줘야 하기 때문에 {중괄호}로 감싸야한다.
+            return <li key={index}>{item}</li>
+            /**
+             * 콘솔창 메시지 : Each child in a list should have a unique "key" prop.
+             * 
+             * <li> 태그에는 원래 key 속성이 없으며 React 만을 위한 내부 식별자
+             * HTML DOM에도 남지 않는다. = React 내부에서만 key 속성이 쓰인다.
+             * 
+             * key prop?
+             * React가 리스트를 렌더링할 때 Virtual DOM을 비교(diffing)해야 하는데, 그때 각 항목을 고유하게 식별할 수 있어야 한다.
+             * 고유값이 없으면 React는 "리스트 전체를 다시 그려야 하나?" 고민하면서 불필요한 재렌더링이 발생할 수 있다.
+             * 이를 위해 React는 key라는 특별한 prop을 도입했다.
+             */
+          })
+        }
+      </ul>
     </div>
   );
 }
